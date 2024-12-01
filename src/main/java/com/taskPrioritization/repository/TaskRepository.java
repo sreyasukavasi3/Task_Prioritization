72e0a44b0d58014b4e5ea1fc9cbc5f91a44d7cec
Sreya <33253396+sreyasukavasi3@users.noreply.github.com>
2024-11-30 21:38:49 -0800

    Update TaskRepository.java

--- src/main/java/com/taskPrioritization/repository/TaskRepository.java
+++ src/main/java/com/taskPrioritization/repository/TaskRepository.java
@@ -1,10 +1,15 @@
 package com.taskPrioritization.repository;
 
-import com.taskPrioritization.Task;
+import com.taskPrioritization.model.Task;
+import com.taskPrioritization.model.Task.Status;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByStatus(Status status);
 
 }
